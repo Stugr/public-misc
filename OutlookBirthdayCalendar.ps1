b774428b73d1ca39a6c164d04f212d8fbb08eb6e
Nick Sturgess <nick@stugr.com>
2020-05-28 11:09:26 +1000

    Added $whatIf

--- OutlookBirthdayCalendar.ps1
+++ OutlookBirthdayCalendar.ps1
@@ -1,5 +1,10 @@
 ﻿# how long to wait betweeen clearing and setting birthday
 $waitTime = 60
+
+# toggle this to $false to actually run
+$whatIf = $true
+
+# optional to turn off if you want to keep the additional recurring calendar entries in the normal calendar
 $deleteRecurringCalendarEntry = $true
 
 # default no birthday date of 4501
@@ -15,14 +20,25 @@ $namespace = $outlook.GetNamespace("MAPI")
 
 # send and receive if outlook is in cached exchange mode
 function Send-AndReceive {
-    if ($outlook.session.Stores | ? { $_.IsCachedExchange }) {
-        ""
-        "Send and receive"
-        Start-Job { $namespace.SendAndReceive($false) } | Wait-Job -Timeout 15 | out-null
+    param(
+        [switch]$force=$false
+    )
+
+    # if not running in whatif mode
+    if (-not $whatIf -or $force) {
+        # if cached exchange
+        if ($outlook.session.Stores | ? { $_.IsCachedExchange }) {
+            Write-Verbose "Send and receive"
+            Start-Job { $namespace.SendAndReceive($false) } | Wait-Job -Timeout 15 | out-null
+        }
     }
 }
 
-Send-AndReceive
+if ($whatIf) {
+    Write-Warning "Running in whatIf mode - actions won't actually be performed"
+}
+
+Send-AndReceive -force
 
 # get all contacts which have a birthday set which isn't the default no birthday date of 4501
 $contacts = $outlook.session.GetDefaultFolder($olFolders::olFolderContacts).items | ? { $_.birthday -ne $noBirthdayDate -and $_.birthday } | sort Fullname
@@ -37,8 +53,10 @@ Write-Verbose "Clearing birthdays"
     # if second run, then send and receive and then sleep to allow outlook.com's backend to catch up
     if ($runCount -eq 1) {
         Send-AndReceive
-        sleep $waitTime
         Write-Verbose "Waiting for $waitTime seconds"
+        if (-not $whatIf) {
+            sleep $waitTime
+        }
         Write-Verbose "Setting birthdays back to original values"
     }
 
@@ -55,34 +73,38 @@ Write-Verbose "Clearing birthdays"
 
         # output what we are doing
         $contact | select Subject, Birthday, @{N='SetBirthdayTo';E={$setBirthday}}
-            
+        
         # change birthday and save
-        $contact.Birthday = $setBirthday
-        $contact.save()
+        if (-not $whatIf) {
+            $contact.Birthday = $setBirthday
+            $contact.save()
+        }
     }
 }
 
 Send-AndReceive
 
-# if calendar entries should be deleted
-if ($deleteRecurringCalendarEntry) {
-    # get recurring calendar entries
-    $cal = $outlook.session.GetDefaultFolder($olFolders::olFolderCalendar).items | ? { $_.IsRecurring }
+# get recurring calendar entries
+$cal = $outlook.session.GetDefaultFolder($olFolders::olFolderCalendar).items | ? { $_.IsRecurring }
 
-    # loop through contacts
-    & {
-        foreach ($contact in $contacts) {
-            # find calendar entries that match 
-            if ($foundCalEntry = $cal | ? { $_.subject -eq "$($contact.subject)'s Birthday"} ) {
-                $foundCalEntry | select subject, start
+# loop through contacts
+& {
+    foreach ($contact in $contacts) {
+        # find calendar entries that match 
+        if ($foundCalEntry = $cal | ? { $_.subject -eq "$($contact.subject)'s Birthday"} ) {
+            $foundCalEntry | select subject, start
 
-                # delete calendar entry
+            # if calendar entries should be deleted
+            if ($deleteRecurringCalendarEntry -and -not $whatIf) {
                 $foundCalEntry.Delete()
             }
         }
-    } | ft -auto
+    }
+} | ft -auto
 
+# dont need the final send and receive if cal entries weren't touched
+if ($deleteRecurringCalendarEntry) {
     Send-AndReceive
-}
+}   
 
 $VerbosePreference = $oldVerbosePreference
